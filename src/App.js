ff6064cf83d84e4a2f5de3eba9863f08669efc18
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2019-08-14 17:11:57 +0200

    ListWrapper added

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ListWrapper from './components/ListWrapper/ListWrapper'
+
+const App = () => (
+    <div>
+        <ListWrapper/>
+    </div>
+);
+
+export default App;
---
+++ src/components/ListWrapper/ListWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListWrapper = () =>(
+    <ul>
+        <li>list item 1</li>
+        <li>list item 2</li>
+        <li>list item 3</li>
+    </ul>
+)
+
+export default ListWrapper

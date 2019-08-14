6cbcd34c1fa9dca83669e9b9f46aae2faae71ee7
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2019-08-14 18:07:31 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
 import './index.css'
+import ListItem from './components/ListWrapper/ListItem/ListItem';
 
 const App = () => (
     <div>
-        <ListWrapper/>
+        <ListItem/>
     </div>
 );
 
--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './ListItem.css';
+import danAbramovImage from '../../../assets/images/danabramov.jpg'
 
 
 const ListItem = () => (
     <li className = "listItemWrapper">
-        item1
+        <img src={danAbramovImage}/>
+        <div>
+            <h2>Dan Abramov</h2>
+            <p>It is a good idea to think of your PC as an office. It stores files, programs, pictures. This can be compared to an actual office's files, machines and decorations.</p>
+            <button>visit twitter page</button>
+        </div>
+
     </li>
 );
 

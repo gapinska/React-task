6bc6cbf0153e104d7aa766a92b3c79f614ca8d17
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2019-08-19 15:27:29 +0200

    classes added

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -4,12 +4,12 @@ import danAbramovImage from '../../../assets/images/danabramov.jpg'
 
 
 const ListItem = () => (
-    <li className = "listItemWrapper">
-        <img src={danAbramovImage}/>
+    <li className = "listItem__wrapper" >
+        <img src={danAbramovImage} className="listItem__image" />
         <div>
-            <h2>Dan Abramov</h2>
-            <p>It is a good idea to think of your PC as an office. It stores files, programs, pictures. This can be compared to an actual office's files, machines and decorations.</p>
-            <button>visit twitter page</button>
+            <h2 className= "listItem__name">Dan Abramov</h2>
+            <p className="listItem__description">It is a good idea to think of your PC as an office. It stores files, programs, pictures. This can be compared to an actual office's files, machines and decorations.</p>
+            <button className="listItem__button ">visit twitter page</button>
         </div>
 
     </li>

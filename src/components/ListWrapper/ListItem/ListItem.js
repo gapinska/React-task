6fdcb0e2743a4ff9088a975cb9f33c03a39e77b0
gapinska <gapinska.mon@gmail.com>
2019-08-21 14:14:35 +0200

    scss added

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './ListItem.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./ListItem.module.scss";
 
-
-
-const ListItem = ({
-    name,
-    description,
-    image,
-    twitterLink
-}) => (
-    <li className = "listItem__wrapper" >
-        <img 
-            src={image} 
-            className="listItem__image"
-            alt={name}
-            />
-        <div>
-            <h2 className= "listItem__name">{name}</h2>
-            <p className="listItem__description">{description}}</p>
-            <a href={twitterLink} className="listItem__button " target="_blank">visit twitter page</a >
-        </div>
-
-    </li>
+const ListItem = ({ image, name, description, twitterLink }) => (
+  <li className={styles.wrapper}>
+    <img src={image} className={styles.image} alt={name} />
+    <div>
+      <h2 className={styles.name}>{name}</h2>
+      <p className={styles.description}>{description}</p>
+      <a
+        href={twitterLink}
+        target="_blank"
+        className={styles.button}
+        rel="noopener noreferrer"
+      >
+        visit twitter page
+      </a>
+    </div>
+  </li>
 );
+
 ListItem.propTypes = {
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    image: PropTypes.string,
-    twitterLink: PropTypes.string.isRequired
-}
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  twitterLink: PropTypes.string.isRequired,
+};
+
 ListItem.defaultProps = {
-    description: 'One of the react creators' 
-}
-
-
+  description: "One of the React creators",
+};
 
 export default ListItem;
+

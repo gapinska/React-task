70e7eb75c7c3a1abd7216c2fbcd2ab2b2ab50838
gapinska <gapinska.mon@gmail.com>
2019-08-24 19:47:02 +0200

    btton as a single component added to the ListItem

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-const Button = ({children}) =>(
+const Button = ({children, href}) =>(
+    <>
+{
+    href ?
+    (
+        <a
+        href={href}        
+        target="_blank"
+        className={styles.button}
+        rel="noopener noreferrer"
+      >
+        {children}
+      </a>
+    ) : (
+
     <button className={styles.button}>
         {children}
     </button>
+
+    )
+
+}
+</>
 )
 
 

806c7655954cd5ae6085014f6fe68674a326c16c
gapinska <gapinska.mon@gmail.com>
2019-08-27 20:10:54 +0200

    modal finished

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,91 +1,88 @@
 import React from "react";
 import styles from "./Form.module.scss";
-import Input from "../Input/Input"
-import Button from '../Button/Button'
-import Title from '../Title/Title'
+import Input from "../Input/Input";
+import Button from "../Button/Button";
+import Title from "../Title/Title";
+import Radio from "./FormRadio";
 
-const types= {
-  twitter: 'twitter',
-  article: 'article',
-  note: 'note'
-}
+const types = {
+  twitter: "twitter",
+  article: "article",
+  note: "note",
+};
 
-const description = {
-  twitter: 'Twitter account',
-  article: 'Article',
-  note: 'Note'
-}
+const descriptions = {
+  twitter: "favorite Twitter account",
+  article: "Article",
+  note: "Note",
+};
 
 class Form extends React.Component {
-
-  state= {
+  state = {
     activeOption: types.twitter,
-  }
+  };
 
-  handleRadioButtonChange =(type) =>{
+  handleRadioButtonChange = type => {
     this.setState({
       activeOption: type,
-    })
+    });
+  };
 
-  }
+  render() {
+    const { activeOption } = this.state;
 
-  render(){
     return (
-    <div className={styles.wrapper}>
-    <Title>Add new {descriptions[this.state.activeOption]}</Title>
-    <form autoComplete="off" className={styles.form} onSubmit={this.props.submitFn}>
+      <div className={styles.wrapper}>
+        <Title>Add new {descriptions[activeOption]}</Title>
+        <form
+          autoComplete="off"
+          className={styles.form}
+          onSubmit={this.props.submitFn}
+        >
+          <div className={styles.formOptions}>
+            <Radio
+              id={types.twitter}
+              checked={activeOption === types.twitter}
+              changeFn={() => this.handleRadioButtonChange(types.twitter)}
+            >
+              Twitter
+            </Radio>
+            <Radio
+              id={types.article}
+              checked={activeOption === types.article}
+              changeFn={() => this.handleRadioButtonChange(types.article)}
+            >
+              Article
+            </Radio>
+            <Radio
+              id={types.note}
+              checked={activeOption === types.note}
+              changeFn={() => this.handleRadioButtonChange(types.note)}
+            >
+              Note
+            </Radio>
+          </div>
+          <Input
+            name="name"
+            label={activeOption === types.twitter ? "Twitter Name" : "Title"}
+            maxLength={30}
+          />
+          {activeOption !== types.note ? (
+            <Input
+              name="link"
+              label={activeOption === types.twitter ? "Twitter Link" : "Link"}
+            />
+          ) : null}
 
-      <input
-      id={types.twitter}
-      type="radio"
-      checked={this.state.activeOption === types.twitter}
-      onChange={() => this.handleRadioButtonChange(types.twitter)}/>
-      <label for={types.twitter}>Twitter</label>
-
-      <input
-      id={types.article}
-      type="radio"
-      checked={this.state.activeOption === types.article}
-      onChange={() => this.handleRadioButtonChange(types.article)}/>
-      <label for={types.article}>Article</label>
-
-      <input
-      id={types.note}
-      type="radio"
-      checked={this.state.activeOption === types.note}
-      onChange={() => this.handleRadioButtonChange(types.note)}/>
-      <label for={types.note}>Note</label>
-
-      <Input
-        name="name"
-        label="Name"
-        maxLength={30}      
-      />
-      <Input
-        name="link"
-        label="Twitter link"
-        maxLength={30}      
-      />
-      <Input
-        name="image"
-        label="Image"
-        maxLength={30}
-      />
-      <Input
-        tag="textarea"
-        name="description"
-        label="Description"
-      />
-
-      <Button>add new item</Button>
-     
-      
-    </form>
-    </div>
-    )
+          {activeOption === types.twitter ? (
+            <Input name="image" label="Image" />
+          ) : null}
+          <Input tag="textarea" name="description" label="Description" />
+          <Button>add new item</Button>
+        </form>
+      </div>
+    );
   }
 }
-  
-
 
 export default Form;

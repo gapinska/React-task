509948077beed80f9a7f95ffac1056c90585d2c4
Monika Gapińska <gapinska.mon@gmail.com>
2020-05-17 15:16:52 +0200

    react - tasks added

---
+++ tasks/app1.js
@@ -0,0 +1,11 @@
+class CheckBoxAgeConfirmation extends React.Component {
+    render() {
+        return (
+            <>
+            <h1>Buy a ticket!</h1>
+            <input type="checkbox" id="age"/>
+            <label htmlFor="age"></label>
+            </>
+        )
+    }
+}

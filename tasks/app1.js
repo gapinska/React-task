8792dedbc305440049a767911e7aa8cee728652c
Monika Gapińska <gapinska.mon@gmail.com>
2020-05-17 15:18:30 +0200

    checkbox, display message

--- tasks/app1.js
+++ tasks/app1.js
@@ -1,11 +1,30 @@
+const PositiveMessage = () => <p>You are allowed to watch the movie.</p>
+const NegativeMessage = () => <p>You are not allowed to watch the movie</p>
+
 class CheckBoxAgeConfirmation extends React.Component {
+    state= {
+        isConfirmed: false
+    }
+
+    handleCheckboxChange = () => {
+        this.setState({
+            isConfirmed: !this.state.isConfirmed
+        })
+    }
+
+    displayMessage= () => this.state.isConfirmed ? <PositiveMessage/> : <NegativeMessage/>
+    
+
     render() {
         return (
             <>
             <h1>Buy a ticket!</h1>
-            <input type="checkbox" id="age"/>
-            <label htmlFor="age"></label>
+            <input type="checkbox" id="age" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed}/>
+            <label htmlFor="age">I am at least 16 years old</label>
+            {this.displayMessage()}
             </>
         )
     }
 }
+
+ReactDOM.render(<CheckBoxAgeConfirmation/>, document.getElementById('root'))

d717b19f139bb4737666fdf0fb30055b69a5e1aa
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2020-05-18 11:33:39 +0200

    cart - application structure and logic

---
+++ tasks/app2.js
@@ -0,0 +1,49 @@
+import React from "react"
+import "./App.css"
+
+class App extends React.Component {
+  state = {
+    availableProducts: 7,
+    shoppingCart: 0,
+  }
+
+  handleRemoveFromCart = () => {
+    if (this.state.shoppingCart !== 0) {
+      this.setState({
+        shoppingCart: this.state.shoppingCart - 1,
+      })
+    }
+  }
+
+  handleAddToCart = () => {
+    this.setState({
+      shoppingCart: this.state.shoppingCart + 1,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <button
+          disabled={this.state.shoppingCart ? false : true}
+          onClick={this.handleRemoveFromCart}
+        >
+          -
+        </button>
+        <span> {this.state.shoppingCart} </span>
+        <button
+          disabled={
+            this.state.shoppingCart === this.state.availableProducts
+              ? true
+              : false
+          }
+          onClick={this.handleAddToCart}
+        >
+          +
+        </button>
+      </>
+    )
+  }
+}
+
+export default App

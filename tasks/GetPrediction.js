e6b10d77ce270743376e9db208bfc2dc44c3e764
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2020-05-21 20:23:07 +0200

    draw prediction task

---
+++ tasks/GetPrediction.js
@@ -0,0 +1,64 @@
+import React from "react"
+
+const ShowPrediction = (props) => <div>{props.predictionText}</div>
+
+class GetPrediction extends React.Component {
+  state = {
+    predictionText: "",
+    predictionPicked: false,
+  }
+
+  static defaultProps = {
+    predictions: [
+      {
+        id: 1,
+        text: "This is the first prediction",
+      },
+      {
+        id: 2,
+        text: "This is the second prediction",
+      },
+      {
+        id: 3,
+        text: "This is the third predicttion",
+      },
+    ],
+  }
+
+  handleShowPrediction = () => {
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(1)
+      max = Math.floor(3)
+      return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+    const numberOfPredictions = this.props.predictions.length
+    const randomId = getRandomIntInclusive(1, numberOfPredictions)
+    const predictionPicked = this.props.predictions.filter(
+      (prediction) => prediction.id === randomId
+    )
+
+    const prediction = predictionPicked.map((prediction) => {
+      const predictionText = prediction.text
+      return predictionText
+    })
+
+    this.setState({
+      predictionPicked: true,
+      predictionText: prediction,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={this.handleShowPrediction}>Wylosuj wróżbę</button>
+        {this.state.predictionPicked ? (
+          <ShowPrediction predictionText={this.state.predictionText} />
+        ) : null}
+      </>
+    )
+  }
+}
+
+export default GetPrediction

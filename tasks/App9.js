1855db6c3455e0965880d9c8c1a310f79bc51b19
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2020-06-05 12:17:58 +0200

    object XMLHttpRequest

---
+++ tasks/App9.js
@@ -0,0 +1,34 @@
+// Obiekt XMLHttpRequest
+import React, { Component } from "react"
+
+class App extends Component {
+  state = {
+    users: [],
+  }
+
+  componentDidMount() {
+    const xhr = new XMLHttpRequest()
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true)
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const users = JSON.parse(xhr.response)
+        this.setState({
+          users,
+        })
+      }
+    }
+    xhr.send(null)
+  }
+
+  render() {
+    const users = this.state.users.map((user) => (
+      <div key={user.id}>
+        <h4>{user.name}</h4>
+        <p>{user.address.city}</p>
+      </div>
+    ))
+    return <div>{users}</div>
+  }
+}
+
+export default App

bedae03d2096f4067fac751e0bedddba25ee03f7
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2020-06-09 15:48:27 +0200

    Update UsersList.js

--- tasks/fetch_task/UsersList.js
+++ tasks/fetch_task/UsersList.js
@@ -2,10 +2,10 @@ import React from "react"
 
 const UsersList = (props) => {
   const users = props.users.map((user) => (
-    <div key={user.login.uuid}>
+    <li key={user.login.uuid}>
       <h4>{`${user.name.title} ${user.name.last}`}</h4>
       <p>{user.email}</p>
-    </div>
+    </li>
   ))
   return <ul>{users}</ul>
 }

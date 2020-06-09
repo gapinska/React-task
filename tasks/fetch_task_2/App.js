ff7132a91bc8bc44ae1b3b79c03de9af1e7044bc
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2020-06-09 16:09:02 +0200

    fetch task
    onclick- add user

---
+++ tasks/fetch_task_2/App.js
@@ -0,0 +1,44 @@
+// Fetch w praktyce – cz. 1: wyświetlanie 5 losowych użytkowników po kliknięciu
+
+import React, { Component } from "react"
+import "./App.css"
+import UsersList from "./UsersList"
+import ButtonFetchUsers from "./ButtonFetchUsers"
+
+const API = "https://randomuser.me/api/?results=5"
+
+class App extends Component {
+  state = {
+    users: [],
+  }
+
+  handleDateFetch = () => {
+    fetch(API)
+      .then((response) => {
+        if (response.ok) {
+          return response
+        }
+        throw Error(response.status)
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        const user = data.results;
+        this.setState(prevState => {
+          users: prevState.push(user),
+        })
+      })
+      .catch((error) => console.log(error))
+  }
+
+  render() {
+    const users = this.state.users
+    return (
+      <div>
+        <ButtonFetchUsers click={this.handleDateFetch} />
+        {users ? <UsersList users={users} /> : users}
+      </div>
+    )
+  }
+}
+
+export default App

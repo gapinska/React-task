394c57d7df82709c40e4fccc8318590283a9465c
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2020-06-09 15:38:16 +0200

    users shown

--- tasks/fetch_task/UsersList.js
+++ tasks/fetch_task/UsersList.js
@@ -1,12 +1,13 @@
 import React from "react"
 
 const UsersList = (props) => {
-  console.log(props)
-  return (
-    <ul>
-      <li>Użytkownik1</li>
-    </ul>
-  )
+  const users = props.users.map((user) => (
+    <div key={user.login.uuid}>
+      <h4>{`${user.name.title} ${user.name.last}`}</h4>
+      <p>{user.email}</p>
+    </div>
+  ))
+  return <ul>{users}</ul>
 }
 
 export default UsersList
